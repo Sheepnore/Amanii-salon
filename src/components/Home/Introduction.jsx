7e7d3973dfec42d50c943d67211fcefd07731eab
Sheepnore <a83840066@gmail.com>
2024-12-26 13:26:09 +0800

    Refactor Introduction component and styles: Updated layout by consolidating subheadings into a single paragraph, renamed CSS classes for clarity, and changed background styling to use an image. Removed unused image elements and adjusted padding for improved design consistency.

--- src/components/Home/Introduction.jsx
+++ src/components/Home/Introduction.jsx
@@ -7,19 +7,13 @@ function Introduction() {
     <div className="introduction" id="introduction">
       <div className="intro-container">
         <div className="text-container">
-          <div className="backgroundColor-layer">
+          <div className="background-container">
             <h1 className="heading">アマンイ 阿曼玉沙龍</h1>
-            <div className="subheadings">
-              <p className="subheading-two">
-                在台灣直營的日式沙龍，秉持日本職人精神，十年來以為數千位客人提供專業的髮型設計。
-              </p>
-            </div>
+            <p className="subheading">
+              在台灣直營的日式沙龍，秉持日本職人精神，十年來以為數千位客人提供專業的髮型設計。
+            </p>
             <CallToActionBtn></CallToActionBtn>
           </div>
-        </div>
-        <div className="img">
-          <img src={introimg} width={1000} height={500} />
-          <div className="overlay"></div>
         </div>
       </div>
     </div>
