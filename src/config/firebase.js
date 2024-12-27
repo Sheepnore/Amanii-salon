0fdd993049b8e641932a28b089a76eef6241443a
Sheepnore <a83840066@gmail.com>
2024-12-27 22:40:33 +0800

    Bug solved: Unable to connect firebase services. Solution: remove/comment out connectAuthEmulator and connectFirestoreEmulator methods

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth, GoogleAuthProvider } from "firebase/auth";
 import {connectFirestoreEmulator, getFirestore} from 'firebase/firestore';
 
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -16,34 +17,14 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider()
 
-connectAuthEmulator(auth,'http://localhost:9099');
-connectFirestoreEmulator(db,'localhost',8080)
+// connectAuthEmulator(auth,'http://localhost:9099');
+// connectFirestoreEmulator(db, 'localhost', 8080);
+  
 
 
 
-// export async function getUserData() {
-//   const user = auth.currentUser;
 
-//   if (user) {
-//     try {
-//       // Get the ID token
-//       const idToken = await user.getIdToken();
-//       // User data
-//       const userData = {
-//         name: user.displayName,
-//         email: user.email,
-//         photo: user.photoURL,
-//         id: user.uid,
-//       };
-//       console.log(userData);
-//       return userData;
-//     } catch (error) {
-//       console.error("Error getting user data:", error);
-//     }
-//   } else {
-//     console.log("No user is signed in");
-//   }
-// }
+
 
 
 
